b040f552a9f52b898b926b8f4c46bcf045bc66fa
AhamedAliNishad <ahamedalinishad.j@syncfusion.com>
2024-12-20 19:06:32 +0530

    Committed the sample

---
+++ FontIconColor/FontIconColor/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FontIconColor
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FontIconColor/FontIconColor/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿namespace FontIconColor
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Tab1 : ContentPage
+    {
+        public Tab1()
+        {
+            IconImageSource = new FontImageSource
+            {
+                FontFamily = "Ionicons",
+                Glyph = "\uf30c",
+                Size = 15
+            };
+            Title = "Tab 1";
+            var verticalStackLayout = new VerticalStackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children =
+    {
+        new Label
+        {
+            HorizontalOptions = LayoutOptions.Center,
+            Text = "Tab 1"
+        }
+    }
+            };
+            Content = verticalStackLayout;
+        }
+    }
+
+    public class Tab2 : ContentPage
+    {
+        public Tab2()
+        {
+            IconImageSource = new FontImageSource
+            {
+                FontFamily = "Ionicons",
+                Glyph = "\uf30c",
+                Color = Colors.Green, // Unicode character for the glyph
+                Size = 15
+            };
+            Title = "Tab 2";
+            var verticalStackLayout = new VerticalStackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children =
+    {
+        new Label
+        {
+            HorizontalOptions = LayoutOptions.Center,
+            Text = "Tab 2"
+        }
+    }
+            };
+            Content = verticalStackLayout;
+        }
+    }
+
+}
